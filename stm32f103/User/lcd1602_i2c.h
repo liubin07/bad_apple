2186164ee8afb5860ac2ff4da95b47e3466d5be6
liubin <liubin07@163.com>
2018-06-02 23:05:04 +0800

    Bad Apple with subtitles

--- stm32f103/User/lcd1602_i2c.h
+++ stm32f103/User/lcd1602_i2c.h
@@ -3,6 +3,6 @@
 
 void LCD1602_I2C_Config(void);
 void LCD1602_I2C_Show_Str(char *str0, char *str1);
-void LCD1602_I2C_Play(uint32_t frame);
+void LCD1602_I2C_Play(uint32_t frame, uint8_t mode);
 
 #endif
--- stm32f103/User/main.c
+++ stm32f103/User/main.c
@@ -7,8 +7,36 @@
 #include "led.h"
 #include "lcd1602_i2c.h"
 
+//=============================================================================
+// Mode switch -> PC14
+//=============================================================================
+#define SWITCH_CLK     RCC_APB2Periph_GPIOC
+#define SWITCH_PIN     GPIO_Pin_14
+#define SWITCH_PORT    GPIOC
+
+void Switch_Config(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  RCC_APB2PeriphClockCmd(SWITCH_CLK, ENABLE);
+
+  GPIO_InitStructure.GPIO_Pin = SWITCH_PIN;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
+  GPIO_Init(SWITCH_PORT, &GPIO_InitStructure);
+}
+
+uint8_t Switch_State(void)
+{
+  if (GPIO_ReadInputDataBit(SWITCH_PORT, SWITCH_PIN) == Bit_SET) {
+    return 1;
+  }
+  return 0;
+}
+
 int main(void)
 {
+  uint8_t mode = 0;
   uint32_t prev_tick = 0;
   /* fps = 5, interval = 200ms */ 
   uint32_t interval = 200;
@@ -17,16 +45,19 @@ int main(void)
 
   SysTick_Init_Config();
   LED_Config();
+  Switch_Config();
   LCD1602_I2C_Config();
 
   LED_Off();
+  mode = Switch_State();
+
   delay_ms(1000);
   LCD1602_I2C_Show_Str("", "");
   delay_ms(1000);
 
   prev_tick = HAL_GetTick();
   while(1) {
-    LCD1602_I2C_Play(frame);
+    LCD1602_I2C_Play(frame, mode);
 
     frame++;
     delay_until_ms(&prev_tick, interval);
