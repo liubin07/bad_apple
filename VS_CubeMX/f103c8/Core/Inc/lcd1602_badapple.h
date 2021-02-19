9e641f036ef32fb8c51e7fec67bba4df457248e1
liubin <liubin07@163.com>
2021-02-19 21:19:33 +0800

    stm32 code by using CubeMX and vscode

---
+++ VS_CubeMX/f103c8/Core/Inc/lcd1602_badapple.h
@@ -0,0 +1,6 @@
+#ifndef __LCD1602_BADAPPLE_H
+#define __LCD1602_BADAPPLE_H
+
+void lcd1602_i2c_play(uint32_t frame, uint8_t mode);
+
+#endif
---
+++ VS_CubeMX/f103c8/Core/Inc/lcd1602_i2c.h
@@ -0,0 +1,44 @@
+#ifndef __LCD1602_I2C_H
+#define __LCD1602_I2C_H
+
+// commands
+#define LCD_CLEARDISPLAY   0x01
+#define LCD_RETURNHOME     0x02
+#define LCD_ENTRYMODESET   0x04
+#define LCD_DISPLAYCONTROL 0x08
+#define LCD_CURSORSHIFT    0x10
+#define LCD_FUNCTIONSET    0x20
+#define LCD_SETCGRAMADDR   0x40
+#define LCD_SETDDRAMADDR   0x80
+
+// flags for function set
+#define LCD_8BITMODE       0x10
+#define LCD_4BITMODE       0x00
+#define LCD_2LINE          0x08
+#define LCD_1LINE          0x00
+#define LCD_5x10DOTS       0x04
+#define LCD_5x8DOTS        0x00
+
+// flags for display on/off control
+#define LCD_DISPLAYON      0x04
+#define LCD_DISPLAYOFF     0x00
+#define LCD_CURSORON       0x02
+#define LCD_CURSOROFF      0x00
+#define LCD_BLINKON        0x01
+#define LCD_BLINKOFF       0x00
+
+// flags for display entry mode
+#define LCD_ENTRYRIGHT     0x00
+#define LCD_ENTRYLEFT      0x02
+#define LCD_ENTRYSHIFTINCREMENT 0x01
+#define LCD_ENTRYSHIFTDECREMENT 0x00
+
+#define LCD_BACKLIGHT      0x08
+#define LCD_NOBACKLIGHT    0x00
+
+void lcd1602_i2c_init(void);
+void lcd1602_i2c_show_str(char *str0, char *str1);
+void lcd1602_command(uint8_t data);
+void lcd1602_write(uint8_t data);
+
+#endif
